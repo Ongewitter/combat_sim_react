1132c800f87c073d841209e05957a0a0ff389b18
Cedric <cedric@Cedrics-MBP.lan>
2021-09-06 17:04:19 +0200

    Use stnadardised and styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+import logo from './logo.svg';
+import CharacterCreate from './character/create';
+import Button from './components/Button';
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [creating, setCreating] = React.useState(false);
 
   React.useEffect(() => {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/combat`)
       .then((res) => res.json())
       .then((data) => setData(data.fun));
   }, []);
+  
+  function handleCreateCharacter() {
+    setCreating(!creating);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+      </AppHeader>
+      <div>
+        <Button onClick={() => handleCreateCharacter()}>
+          Create a character
+        </Button>
+        The user is <b>{creating ? 'creating' : 'not creating'}</b> a character.
+        {creating ? <CharacterCreate/> : ''}
+      </div>
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+`;
