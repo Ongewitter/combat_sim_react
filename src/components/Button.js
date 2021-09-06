d4fc338f51aa7272440499a79996960eae801414
Cedric <cedric@Cedrics-MBP.lan>
2021-09-06 17:03:12 +0200

    Add standard styled button component

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  text-align: center;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+function Button(props) {
+  return(
+    <StyledButton>
+      {props.children}
+    </StyledButton>
+  )
+}
+
+export default Button;
