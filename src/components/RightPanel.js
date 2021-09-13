5bc16238e1afced740c84760d353b213af53f9e0
Cedric <cedric@Cedrics-MBP.lan>
2021-09-13 17:03:16 +0200

    Add RightPanel and styling. Ugly and basic for now

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from 'styled-components';
 
-function RightPanel() {
+function RightPanel(props) {
   return (
-    <div className="rightpanel">
-    </div>
+    <RightPanelDiv>
+      {props.children}
+    </RightPanelDiv>
   );
 }
 
 export default RightPanel;
+
+const RightPanelDiv = styled.div`
+  background-color: white;
+  border: solid 2px black;
+  border-radius: 2px;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 33%;
+`;
