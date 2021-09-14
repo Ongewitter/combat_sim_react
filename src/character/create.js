29c3cc7dc8aab025190d7e0b890bdda707552375
Cedric <cedric@Cedrics-MBP.lan>
2021-09-14 12:51:07 +0200

    let DiceInput handle its thing; Finally get POST started

--- src/character/create.js
+++ src/character/create.js
@@ -1,44 +1,82 @@
 import React from "react";
 import styled from 'styled-components';
 
+import Button from '../components/Button';
 import InputWithLabel from '../components/InputWithLabel';
 import NumberInput from "../components/NumberInput";
 import TextInput from '../components/TextInput';
 import DiceInput from "../components/DiceInput";
 
 function Create() {
+  const [name, setName] = React.useState('');
+  const [hp, setHp] = React.useState(1);
+  const [armor, setArmor] = React.useState(10);
+  const [toHit, setToHit] = React.useState(0);
+  const [damage, setDamage] = React.useState({damage: 0, die: 0});
+  const [bonusDamage, setBonusDamage] = React.useState({damage: 0, die: 0});
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const character = {
+      name: name,
+      hp: hp,
+      armor: armor,
+      toHit: toHit,
+      damage: damage,
+      bonusDamage: bonusDamage
+    }
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/combat`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(character),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <CreateWrapper>
       <CreateHeader>
         Add a new character
       </CreateHeader>
-      <form>
+      <form onSubmit={handleSubmit}>
         <InputWithLabel>
           Name:
-          <TextInput name="name"/>
+          <TextInput name="name" onChange={(e) => setName(e.target.value)}/>
         </InputWithLabel>
         <InputWithLabel>
           HP:
-          <NumberInput name="hp" />
+          <NumberInput name="hp" onChange={(e) => setHp(e.target.value)}/>
         </InputWithLabel>
         {/* TODO Should toggle depending on ruleset */}
         <InputWithLabel>
           Armor:
-          <NumberInput name="armor" />
+          <NumberInput name="armor" onChange={(e) => setArmor(e.target.value)}/>
         </InputWithLabel>
         <InputWithLabel>
           toHit:
-          <NumberInput name="toHit" />
+          <NumberInput name="toHit" onChange={(e) => setToHit(e.target.value)}/>
         </InputWithLabel>
         <InputWithLabel>
           Main attack damage die:
-          <DiceInput name="damage" />
+          <DiceInput name="damage" onChange={setDamage}/>
         </InputWithLabel>
         <InputWithLabel>
           Once-per-turn bonus damage:
-          <DiceInput name="bonus_damage" />
+          <DiceInput name="bonus_damage" onChange={setBonusDamage}/>
         </InputWithLabel>
-        <input type="submit" value="Add character" />
+        <Button type='submit' onClick={() => {}}>
+          Add character
+        </Button>
       </form>
     </CreateWrapper>
   );
