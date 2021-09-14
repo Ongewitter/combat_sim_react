f468898da5ee815a11f3bbbfeed0d0d564e64cf1
Cedric <cedric@Cedrics-MBP.lan>
2021-09-14 12:50:34 +0200

    string up onChange for dice

--- src/components/DiceInput.js
+++ src/components/DiceInput.js
@@ -2,9 +2,34 @@ import React from "react";
 import styled from 'styled-components';
 
 function DiceInput(props) {
+  const [amount, setAmount] = React.useState(0);
+  const [die, setDie] = React.useState(0);
+
+  function amountChanged(value) {
+    setAmount(value)
+    props.onChange({
+      amount: amount,
+      die: die
+    });
+  }
+
+  function dieChanged(value) {
+    setDie(value)
+    props.onChange({
+      amount: amount,
+      die: die
+    });
+  }
+
   return (
     <S.DiceDiv>
-      <S.Input type="number" name={props.name + '_amount'} /> 🎲 <S.Input type="number" name={props.name + '_die'} />
+      <S.Input
+        type="number"
+        name={props.name + '_amount'}
+        onChange={(e) => amountChanged(e.target.value)}/> 🎲  <S.Input
+                  type="number"
+                  name={props.name + '_die'}
+                  onChange={(e) => dieChanged(e.target.value)} />
     </S.DiceDiv>
   );
 }
