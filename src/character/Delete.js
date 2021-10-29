9302966d73d2ef603ac24d0bef6dc3d43cb7a9ea
Cedric <cedric@Cedrics-MBP.lan>
2021-10-29 15:30:25 +0200

    Add dummy Delete for later

---
+++ src/character/Delete.js
@@ -0,0 +1,26 @@
+// TODO: Should be called in App.js
+function Delete({ id }) {
+  // function handleDelete(event) {
+  //   event.preventDefault();
+
+  //   const character = {
+  //     id: id
+  //   };
+  //   fetch(`${process.env.REACT_APP_BACKEND_URL}/characters`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(character),
+  //   })
+  //     .then(response => response.json())
+  //     .then(character => {
+  //       onSubmit(character);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // }
+}
+
+export default Delete;
